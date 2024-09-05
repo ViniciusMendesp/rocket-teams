991b4590836a1bcee040760fc0a642bef5a20451
ViniciusMendesp <vmp_tech@outlook.com>
2024-09-05 07:54:11 -0300

    feat: :sparkles: using new components

--- screens/Groups/index.tsx
+++ screens/Groups/index.tsx
@@ -1,10 +1,31 @@
 import { Header } from "@/components/Header";
 import * as S from "./styles";
+import { HighLight } from "@/components/HighLight";
+import { GroupCard } from "@/components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@/components/ListEmpty";
+import { Button } from "@/components/Button";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <S.Container>
       <Header />
+      <HighLight title="Turmas" subtitle="Jogue com sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        }
+      />
+
+      <Button title="Criar nova turma" />
     </S.Container>
   );
 }
