a7c80ba5642bae61d3c82fdc840eca2668345e1c
ViniciusMendesp <vmp_tech@outlook.com>
2024-09-06 16:22:26 -0300

    feat: :sparkles: component player card

---
+++ components/PlayerCard/index.tsx
@@ -0,0 +1,17 @@
+import { ButtonIcon } from "../ButtonIcon";
+import * as S from "./styles";
+
+type PlayerCardProps = {
+  name: string;
+  onRemove: () => void;
+};
+
+export function PlayerCard({ name, onRemove }: PlayerCardProps) {
+  return (
+    <S.Container>
+      <S.Icon name="person" />
+      <S.Name>{name}</S.Name>
+      <ButtonIcon icon="close" type="secondary" onPress={onRemove} />
+    </S.Container>
+  );
+}
